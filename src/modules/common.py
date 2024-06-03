547eb7643bd7b264b4d31eda5dc9f06eb735ebb1
masaPokemon <masa.gongon2011@gmail.com>
2024-06-03 16:14:04 +0900

    Update common.py

--- src/modules/common.py
+++ src/modules/common.py
@@ -11,6 +11,7 @@ def set_pages():
     """
     default_pages = [
         Page("src/01_login.py", "Login/Logout", "🏠"),
+        Page("src/other_pages/02_register_user.py", "Register user", "📝"),
     ]
     after_login_pages = [
         Page("src/other_pages/03_reset_password.py", "Reset password", "🔑"),
