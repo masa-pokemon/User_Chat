3bb05de8760ede9f1416b8b1dd526e2b40cc2d36
masaPokemon <masa.gongon2011@gmail.com>
2024-06-04 13:11:18 +0900

    Update 06_chat.py

--- src/other_pages/06_chat.py
+++ src/other_pages/06_chat.py
@@ -14,7 +14,8 @@ from modules.database import database
 
 username = st.session_state[const.SESSION_INFO_USERNAME]
 if username == "suzuki" or username == "Suzuki" or username == "Yahata" or username == "yahata" or username == "Sakoda" or username == "sakoda" :
-    Chat_ID = st.chat_input("chatID")
+    if Chat_ID := st.chat_input("chatID"):
+        print("ok")
 else:
     CHAT_ID = ""
 persona = None
