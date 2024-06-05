a16b1e7f6e215eb1b17d1e162b4d74ff76fc8a97
masaPokemon <masa.gongon2011@gmail.com>
2024-06-05 12:37:34 +0900

    Update 01_videoChat.py

--- src/other_pages/01_videoChat.py
+++ src/other_pages/01_videoChat.py
@@ -79,9 +79,9 @@ def get_filters():
         "laughing_man": imread_from_url(
             "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3a17e5a4-9610-4fa3-a4bd-cb7d94d6f7e1/darwcty-d989aaf1-3cfa-4576-b2ac-305209346162.png/v1/fill/w_944,h_847,strp/laughing_man_logo_by_aggressive_vector_darwcty-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTE5IiwicGF0aCI6IlwvZlwvM2ExN2U1YTQtOTYxMC00ZmEzLWE0YmQtY2I3ZDk0ZDZmN2UxXC9kYXJ3Y3R5LWQ5ODlhYWYxLTNjZmEtNDU3Ni1iMmFjLTMwNTIwOTM0NjE2Mi5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.5SDBnNZF6ktZM7Mk5gJfpHNQswRba3eqpvUn6FMHyW4"  # noqa: E501
         ),
-        "cat": imread_from_url(_
+        "cat": imread_from_url(
             "https://i.pinimg.com/originals/29/cd/fd/29cdfdf2248ce2465598b2cc9e357579.png"  # noqa: E501
-        )
+        ),
     }
 
 
@@ -90,8 +90,12 @@ def main():
         server_state["webrtc_contexts"] = []
 
     face_cascade = get_face_classifier()
-    
+    filters = get_filters()
+
     filter_type = st.radio(
+        "Select filter type",
+        ("ironman", "laughing_man", "cat"),
+        key="filter-type",
     )
     draw_rect = st.checkbox("Draw rect (for debug)")
 
@@ -107,12 +111,6 @@ def main():
 
         for (x, y, w, h) in faces:
             # Ad-hoc adjustment of the ROI for each filter type
-            if filter_type == "ironman":
-                roi = (x, y, w, h)
-            elif filter_type == "laughing_man":
-                roi = (x, y, int(w * 1.15), h)
-            elif filter_type == "cat":
-                roi = (x, y - int(h * 0.3), w, h)
             overlay_bgra(img, overlay, roi)
 
             if draw_rect:
