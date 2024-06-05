ff8979d16d050b109f4db90e1043134d968578b5
masaPokemon <masa.gongon2011@gmail.com>
2024-06-05 09:45:15 +0900

    Update 08.web_Camera.py

--- src/other_pages/08.web_Camera.py
+++ src/other_pages/08.web_Camera.py
@@ -1,2 +1,9 @@
 import streamlit as st
-import 
+from streamlit_webrtc import webrtc_streamer
+import cv2
+import av #strealing video library
+
+st.title('Streamlit App Test')
+st.write('Hello world')
+
+webrtc_streamer(key='example')
