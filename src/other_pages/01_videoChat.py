e48be707310d2a80a3c04c4f0cd69bf1d9544fbc
masaPokemon <masa.gongon2011@gmail.com>
2024-06-05 11:37:05 +0900

    Update 01_videoChat.py

--- src/other_pages/01_videoChat.py
+++ src/other_pages/01_videoChat.py
@@ -13,7 +13,7 @@ threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=
 def callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
-    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR)
 
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
