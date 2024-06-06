66d3d48b8d3f089ec1e3358e04d8c3179cb015de
masaPokemon <masa.gongon2011@gmail.com>
2024-06-06 11:25:00 +0900

    Update common.py

--- src/modules/common.py
+++ src/modules/common.py
@@ -11,12 +11,12 @@ def set_pages():
     """Set the pages to be shown in the sidebar.
     """
     default_pages = [
-        Page("src/other_pages/app_mcu_filters.py", "camera","📷" )
+        Page("src/other_pages/app_mcu_filters.py", "camera","📷" ),
         Page("src/01_login.py", "Login Logout", "🏠"),
         Page("src/other_pages/02_register_user.py", "Register user", "📝"),
     ]
     after_login_pages = [
-        Page("src/other_pages/app_mcu_filters.py", "camera","📷" )
+        Page("src/other_pages/app_mcu_filters.py", "camera","📷" ),
             
         Page("src/other_pages/03_reset_password.py", "Reset password", "🔑"),
         Page("src/other_pages/04_change_icon.py", "Change icon", "👤"),
