c7629a85634ae48044e033f57bb8355ac9eae3b8
masaPokemon <masa.gongon2011@gmail.com>
2024-06-10 12:11:24 +0900

    Update 06_chat.py

--- src/other_pages/06_chat.py
+++ src/other_pages/06_chat.py
@@ -1,41 +1,30 @@
-# This page is for chat
-import streamlit as st
-from st_pages import add_page_title
-import const
-import datetime
-import os
-from PIL import Image
-import openai
-from streamlit_autorefresh import st_autorefresh
-from modules import common
-from modules.authenticator import common_auth
-from modules.database import database
-import logging
-import math
-import urllib.request
-from pathlib import Path
-from typing import List
-try:
-    from typing import Literal
-except ImportError:
-    from typing_extensions import Literal  # type: ignore
-
-import av
-import cv2
-import numpy as np
-from streamlit_server_state import server_state, server_state_lock
-from streamlit_webrtc import (
-    VideoProcessorBase,
-    WebRtcMode,
-    WebRtcStreamerContext,
-    create_mix_track,
-    create_process_track,
-    webrtc_streamer,
-)
-user_msg = st.chat_input("Enter your message")
 col1, col2 = st.columns(2)
 with col1:
-    
+    import logging
+    import math
+    import urllib.request
+    from pathlib import Path
+    from typing import List
+
+    try:
+        from typing import Literal
+        except ImportError:
+        from typing_extensions import Literal  # type: ignore
+
+    import av
+    import cv2
+    import numpy as np
+    import streamlit as st
+    from streamlit_server_state import server_state, server_state_lock
+    from streamlit_webrtc import (
+        VideoProcessorBase,
+        WebRtcMode,
+        WebRtcStreamerContext,
+        create_mix_track,
+        create_process_track,
+        webrtc_streamer,
+    )
+
     logger = logging.getLogger(__name__)
 
     cv2_path = Path(cv2.__file__).parent
@@ -142,22 +131,22 @@ with col1:
             return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-        def mixer_callback(frames: List[av.VideoFrame]) -> av.VideoFrame:
-            buf_w = 640
-            buf_h = 480
-            buffer = np.zeros((buf_h, buf_w, 3), dtype=np.uint8)
+    def mixer_callback(frames: List[av.VideoFrame]) -> av.VideoFrame:
+        buf_w = 640
+        buf_h = 480
+        buffer = np.zeros((buf_h, buf_w, 3), dtype=np.uint8)
 
-            n_inputs = len(frames)
+        n_inputs = len(frames)
 
-            n_cols = math.ceil(math.sqrt(n_inputs))
-            n_rows = math.ceil(n_inputs / n_cols)
-            grid_w = buf_w // n_cols
-            grid_h = buf_h // n_rows
+        n_cols = math.ceil(math.sqrt(n_inputs))
+        n_rows = math.ceil(n_inputs / n_cols)
+        grid_w = buf_w // n_cols
+        grid_h = buf_h // n_rows
 
-            for i in range(n_inputs):
-                frame = frames[i]
-                if frame is None:
-                    continue
+        for i in range(n_inputs):
+            frame = frames[i]
+            if frame is None:
+                continue
 
             grid_x = (i % n_cols) * grid_w
             grid_y = (i // n_cols) * grid_h
@@ -182,15 +171,15 @@ with col1:
                 img, (window_w, window_h)
             )
 
-            new_frame = av.VideoFrame.from_ndarray(buffer, format="bgr24")
+        new_frame = av.VideoFrame.from_ndarray(buffer, format="bgr24")
 
-            return new_frame
+        return new_frame
 
 
     def main():
         with server_state_lock["webrtc_contexts"]:
             if "webrtc_contexts" not in server_state:
-                iserver_state["webrtc_contexts"] = []
+                server_state["webrtc_contexts"] = []
 
         with server_state_lock["mix_track"]:
             if "mix_track" not in server_state:
@@ -198,85 +187,98 @@ with col1:
                     kind="video", mixer_callback=mixer_callback, key="mix"
                 )
 
-            mix_track = server_state["mix_track"]
+        mix_track = server_state["mix_track"]
 
-            self_ctx = webrtc_streamer(
-                key="self",
-                mode=WebRtcMode.SENDRECV,
-                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-                media_stream_constraints={"video": True, "audio": True},
-                source_video_track=mix_track,
-                sendback_audio=False,
+        self_ctx = webrtc_streamer(
+            key="self",
+            mode=WebRtcMode.SENDRECV,
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            media_stream_constraints={"video": True, "audio": True},
+            source_video_track=mix_track,
+            sendback_audio=False,
+        )
+
+        self_process_track = None
+        if self_ctx.input_video_track:
+            self_process_track = create_process_track(
+                input_track=self_ctx.input_video_track,
+                processor_factory=FaceOverlayProcessor,
+            )
+            mix_track.add_input_track(self_process_track)
+
+            self_process_track.processor.filter_type = st.radio(
+                "Select filter type",
+                ("ironman", "laughing_man", "cat"),
+                key="filter-type",
             )
 
-            self_process_track = None
-            if self_ctx.input_video_track:
-                self_process_track = create_process_track(
-                    input_track=self_ctx.input_video_track,
-                    processor_factory=FaceOverlayProcessor,
-                )
-                mix_track.add_input_track(self_process_track)
+        with server_state_lock["webrtc_contexts"]:
+            webrtc_contexts: List[WebRtcStreamerContext] = server_state["webrtc_contexts"]
+            self_is_playing = self_ctx.state.playing and self_process_track
+            if self_is_playing and self_ctx not in webrtc_contexts:
+                webrtc_contexts.append(self_ctx)
+                server_state["webrtc_contexts"] = webrtc_contexts
+            elif not self_is_playing and self_ctx in webrtc_contexts:
+                webrtc_contexts.remove(self_ctx)
+                server_state["webrtc_contexts"] = webrtc_contexts
 
-                self_process_track.processor.filter_type = st.radio(
-                    "Select filter type",
-                    ("ironman", "laughing_man", "cat"),
-                    key="filter-type",
-                )
-
-            with server_state_lock["webrtc_contexts"]:
-                webrtc_contexts: List[WebRtcStreamerContext] = server_state["webrtc_contexts"]
-                self_is_playing = self_ctx.state.playing and self_process_track
-                if self_is_playing and self_ctx not in webrtc_contexts:
-                    webrtc_contexts.append(self_ctx)
-                    server_state["webrtc_contexts"] = webrtc_contexts
-                elif not self_is_playing and self_ctx in webrtc_contexts:
-                    webrtc_contexts.remove(self_ctx)
-                    server_state["webrtc_contexts"] = webrtc_contexts
-
-                # Audio streams are transferred in SFU manner
-                # TODO: Create MCU to mix audio streams
-                for ctx in webrtc_contexts:
-                    if ctx == self_ctx or not ctx.state.playing:
-                        continue
-                    webrtc_streamer(
-                        key=f"sound-{id(ctx)}",
-                        mode=WebRtcMode.RECVONLY,
-                        rtc_configuration={
-                            "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-                        },
-                        media_stream_constraints={"video": False, "audio": True},
-                        source_audio_track=ctx.input_audio_track,
-                        desired_playing_state=ctx.state.playing,
-                    )
+        # Audio streams are transferred in SFU manner
+        # TODO: Create MCU to mix audio streams
+        for ctx in webrtc_contexts:
+            if ctx == self_ctx or not ctx.state.playing:
+                continue
+            webrtc_streamer(
+                key=f"sound-{id(ctx)}",
+                mode=WebRtcMode.RECVONLY,
+                rtc_configuration={
+                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                },
+                media_stream_constraints={"video": False, "audio": True},
+                source_audio_track=ctx.input_audio_track,
+                desired_playing_state=ctx.state.playing,
+            )
 
 
-            if __name__ == "__main__":
-                import os
+    if __name__ == "__main__":
+        import os
 
-                DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+        DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
 
-                logging.basicConfig(
-                    format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
-                    "%(message)s",
-                    force=True,
-                )
+        logging.basicConfig(
+            format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+            "%(message)s",
+            force=True,
+        )
 
-                logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+        logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
 
-                st_webrtc_logger = logging.getLogger("streamlit_webrtc")
-                st_webrtc_logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+        st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+        st_webrtc_logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
 
-                aioice_logger = logging.getLogger("aioice")
-                aioice_logger.setLevel(logging.WARNING)
+        aioice_logger = logging.getLogger("aioice")
+        aioice_logger.setLevel(logging.WARNING)
 
-                fsevents_logger = logging.getLogger("fsevents")
-                fsevents_logger.setLevel(logging.WARNING)
+        fsevents_logger = logging.getLogger("fsevents")
+        fsevents_logger.setLevel(logging.WARNING)
 
-                main()
-        
+        main()
+
 with col2:
+
+    # This page is for chat
+    import streamlit as st
+    from st_pages import add_page_title
+    import const
+    import datetime
+    import os
+    from PIL import Image
+    import openai
+    from streamlit_autorefresh import st_autorefresh
+    from modules import common
+    from modules.authenticator import common_auth
+    from modules.database import database
+
     CHAT_ID = "0"
-    CHAT_ID = st.text_input("Enter your message")
     persona = None
     llm = None
     use_chatbot = False
@@ -297,10 +299,26 @@ with col2:
     ):
         messages = []
         # Check if chatbot is enabled
+        tmp_use_chatbot = db.get_openai_settings_use_character()
+        if tmp_use_chatbot == 1:
+            persona = db.get_character_persona()[0]
+            messages.append(
+                {"role": "system", "content": CHATBOT_PERSONA.format(persona=persona)}
+            )
+
+            # Get chatbot settings
+            openai_api_key = os.getenv("OPENAI_API_KEY")
+            if openai_api_key is None:
+                openai.api_key = openai_api_key
+                persona = None
+                st.error(
+                    "OPENAI_API_KEY is not set in the environment variables. Please contact the administrator."
+                )
 
         user_infos = {}
         username = st.session_state[const.SESSION_INFO_USERNAME]
         name = st.session_state[const.SESSION_INFO_NAME]
+        user_msg = st.chat_input("Enter your message")
 
         # Show old chat messages
         chat_log = db.get_chat_log(chat_id=CHAT_ID, limit=const.MAX_CHAT_LOGS)
@@ -332,7 +350,7 @@ with col2:
                         )
                     with st.chat_message(
                         log_name, avatar=user_infos[log_username]["image"]
-                        ):
+                    ):
                         st.write(log_name + "> " + log_message)
 
                     if persona is not None:
@@ -411,3 +429,4 @@ with col2:
         )
     else:
         st.error("You are not logged in. Please go to the login page.")
+
