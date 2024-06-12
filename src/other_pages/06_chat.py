292ca5cce4998da3b97a77c8122a37793ec64e91
masaPokemon <masa.gongon2011@gmail.com>
2024-06-12 14:13:29 +0900

    Update 06_chat.py

--- src/other_pages/06_chat.py
+++ src/other_pages/06_chat.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 user_msg = st.chat_input("Enter your message")
-col1, col2 = st.columns(2)
+col1, col2,col3 = st.columns(3)
 with col1:
     import logging
     import math
