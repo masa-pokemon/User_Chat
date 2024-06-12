47656ee9b940bb9bf1e0d3c27491ff0cced4c07a
masaPokemon <masa.gongon2011@gmail.com>
2024-06-12 11:24:25 +0900

    Update 01_login.py

--- src/01_login.py
+++ src/01_login.py
@@ -27,7 +27,7 @@ use_chatbot = args.use_chatbot
 db = database.Database()
 password = stauth.Hasher("pokemon").generate()
 
-db.insert_user_info("suzuki","suzuki@suzuki","suzuki",password,"")
+db.insert_user_info("suzuki","suzuki@suzuki","suzuki", "","")
 current_use_chatbot = db.get_openai_settings_use_character()
 if int(use_chatbot) != current_use_chatbot:
     db.update_openai_settings_use_character(use_chatbot)
