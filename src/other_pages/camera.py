7ee7d2483faa45b94d8c27f41f06d906c2c29b3a
masaPokemon <masa.gongon2011@gmail.com>
2024-06-15 10:44:32 +0900

    Update camera.py

--- src/other_pages/camera.py
+++ src/other_pages/camera.py
@@ -1,68 +1,62 @@
-import streamlit as st  # Streamlitライブラリをインポート
-import edge_tts  # Edge TTSライブラリをインポート
-import os  # OSライブラリをインポート（ファイル操作用）
-import asyncio  # 非同期処理を行うためのライブラリ
-import speech_recognition as sr  # 音声認識のためのライブラリ
+import os
+import speech_recognition as sr
+from datetime import datetime
 
-# Streamlitウェブアプリのタイトルを設定
-st.title('音声作成アプリ')
 
-# 音声認識を行う関数の定義
-def recognize_speech():
-    r = sr.Recognizer()  # 音声認識器のインスタンスを作成
-    mic = sr.Microphone()  # マイクロフォンのインスタンスを作成
-    with mic as source:  # マイクロフォンを音声入力ソースとして使用
-        audio = r.listen(source)  # 音声を聞いて、オーディオデータを取得
+class SpeechRecognizer:
+    """マイクで受け取った音声を認識してファイル出力するクラス
+    """
+    def __init__(self):
+        os.makedirs("./out", exist_ok=True)
+        self.path = f"./out/asr.txt"
+
+        self.rec = sr.Recognizer()
+        self.mic = sr.Microphone()
+        self.speech = []
+        return
+
+    def grab_audio(self) -> sr.AudioData:
+        """マイクで音声を受け取る関数
+
+        Returns:
+            speech_recognition.AudioData: 音声認識エンジンで受け取った音声データ
+        """
+        print("何か話してください...")
+        with self.mic as source:
+            self.rec.adjust_for_ambient_noise(source)
+            audio = self.rec.listen(source)
+        return audio
+
+    def recognize_audio(self, audio: sr.AudioData) -> str:
+        print ("認識中...")
         try:
-            # Googleの音声認識サービスを使用して日本語のテキストに変換
-            return r.recognize_google(audio, language="ja-JP")
+            speech = self.rec.recognize_google(audio, language='ja-JP')
         except sr.UnknownValueError:
-            # 音声が認識できなかった場合
-            return ""
-        except sr.RequestError:
-            # 音声認識サービスへのリクエストに失敗した場合
-            return ""
+            speech = f"#認識できませんでした"
+            print(speech)
+        except sr.RequestError as e:
+            speech = f"#音声認識のリクエストが失敗しました:{e}"
+            print(speech)
+        return speech
 
-# 音声認識を実行するボタンを追加
-if st.button('Record Speech'):
-    recognized_text = recognize_speech()  # 音声認識関数を呼び出し、結果を取得
-else:
-    # セッション状態から以前の認識テキストを取得
-    recognized_text = st.session_state.get('recognized_text', "")
+    def run(self):
+        """マイクで受け取った音声を認識してテキストファイルに出力
+        """
+        while True:
+            audio = self.grab_audio()
+            speech = self.recognize_audio(audio)
 
-# テキスト入力フィールドを追加。認識されたテキストをデフォルト値として設定
-text = st.text_area('Text', recognized_text)
+            if speech == "終わり":
+                print("音声認識終了")
+                break
+            else:
+                self.speech.append(speech)
+                print(speech)
 
-# 音声の声質を選択するためのドロップダウンメニューを追加
-voice = st.selectbox(
-    'Voice', 
-    ['ja-JP-KeitaNeural', 'ja-JP-NanamiNeural', 'en-US-JessaNeural', 
-     'zh-CN-XiaoxiaoNeural', 'zh-CN-YunyangNeural', 'ko-KR-SunHiNeural']
-)
+        with open(self.path, mode='w', encoding="utf-8") as out:
+            out.write(datetime.now().strftime('%Y%m%d_%H:%M:%S') + "\n\n")
+            out.write("\n".join(self.speech) + "\n")
 
-# 音声の速度を調整するためのスライダーを追加
-rate = st.slider('Rate', -100, 100, 20)
-
-# 音声のピッチを調整するためのスライダーを追加
-pitch = st.slider('Pitch', -100, 100, 0)
-
-# 音声ファイルの出力名を設定
-output_file = 'output.mp3'
-
-# 音声生成を実行するボタンを追加
-if st.button('Generate Speech'):
-    rate_str = f"{rate:+}%"  # 速度の設定を文字列に変換
-    pitch_str = f"{pitch:+}Hz"  # ピッチの設定を文字列に変換
-    # 音声合成の設定を行い、ファイルに保存
-    communicate = edge_tts.Communicate(text, voice, rate=rate_str, pitch=pitch_str)
-    asyncio.run(communicate.save(output_file))
-    # 生成された音声をウェブアプリ上で再生
-    st.audio(output_file)
-
-# 音声ファイルが存在する場合、削除ボタンを追加
-if os.path.exists(output_file):
-    if st.button('Delete File'):
-        os.remove(output_file)  # ファイルを削除
-
-# 認識されたテキストをセッション状態に保存
-st.session_state['recognized_text'] = recognized_text
+if __name__ == "__main__":
+    sp = SpeechRecognizer()
+    sp.run()
