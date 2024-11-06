0dd86ef89d5203b4a7903f4166d80409d5151bdc
masaPokemon <masa.gongon2011@gmail.com>
2024-11-06 10:21:53 +0900

    Update const.py

--- src/const.py
+++ src/const.py
@@ -19,13 +19,14 @@ def get_trade_posts():
         posts.append(doc.to_dict())
     return posts
 
-def add_trade_post(title, description, user_name):
+def add_trade_post(title, description, user_name, get_card):
     """新しいトレード投稿をFirestoreに追加"""
     posts_ref = db.collection('trade_posts')
     post_data = {
         'title': title,
         'description': description,
         'user_name': user_name,
+        'get_card': get_card,
         'created_at': firestore.SERVER_TIMESTAMP
     }
     posts_ref.add(post_data)
