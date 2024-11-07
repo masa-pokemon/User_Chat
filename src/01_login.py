0b10779962ade7908319d4f7bfdebf2333d755ef
masaPokemon <masa.gongon2011@gmail.com>
2024-11-07 12:28:58 +0900

    Update 01_login.py

--- src/01_login.py
+++ src/01_login.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 
-if st.button("録音スタート")audio_bytes = audio_recorder()
+if st.button("録音スタート"):
+    audio_bytes = audio_recorder()
 
 if st.button("録音保存"):
     with open("recorded_audio.wav", "wb") as f:
