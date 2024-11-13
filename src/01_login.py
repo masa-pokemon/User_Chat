2a38cf39937e9f9c73a25087e5f316d5e8a0d226
masaPokemon <masa.gongon2011@gmail.com>
2024-11-14 08:19:18 +0900

    Update 01_login.py

--- src/01_login.py
+++ src/01_login.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import yt_dlp
-
-#最高の画質と音質を動画をダウンロードする
-ydl_opts = {
-    'format': 'best',
-    'outtmpl':'video.%(ext)s',
-}
+ydl_opts = {'format': 'best','outtmpl':'video.%(ext)s',}
 text = st.text_input("URL：")
 text1 = ""
 if text != text1:
@@ -13,11 +8,8 @@ if text != text1:
     if os.path.isfile('video.mp4'):
         os.remove('video.mp4')
     text1 = text
-    #動画のURLを指定
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download(text)
-
         video_file = open("video.mp4", "rb")
         video_bytes = video_file.read()
-
         st.video(video_bytes)
