69fbbdda29971c0db6ccdd953bb0ebdf88a0a0f7
masaPokemon <masa.gongon2011@gmail.com>
2024-11-14 08:40:54 +0900

    Update 01_login.py

--- src/01_login.py
+++ src/01_login.py
@@ -1,10 +1,13 @@
-import streamlit as st
 import requests
-from bs4 import BeautifulSoup
-import streamlit.components.v1 as stc
-# Webページを取得して解析する
 
-load_url = "https://www.ymori.com/books/python2nen/test1.html"
-html = requests.get(load_url)
-soup = BeautifulSoup(html.content, "html.parser")
-st.components.v1.html(soup)
+# 取得したいURLを指定
+url = 'https://www.youtube.com/'
+
+# GETリクエストでHTMLを取得
+response = requests.get(url)
+
+# ステータスコードが200 (OK) ならHTMLを表示
+if response.status_code == 200:
+    print(response.text)  # HTML内容を表示
+else:
+    print(f"Failed to retrieve the page. Status code: {response.status_code}")
