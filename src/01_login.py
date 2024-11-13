3500c1ec1633ce4e3287852c3022f2dfb3fcc108
masaPokemon <masa.gongon2011@gmail.com>
2024-11-14 08:32:03 +0900

    Update 01_login.py

--- src/01_login.py
+++ src/01_login.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+import streamlit.components.v1 as stc
 # Webページを取得して解析する
 
 load_url = "https://www.ymori.com/books/python2nen/test1.html"
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
-st.html(soup)
+stc.html(soup)
