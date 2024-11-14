40e20e503c2786ce0a1067f73a701acad4123ff2
masaPokemon <masa.gongon2011@gmail.com>
2024-11-14 09:11:58 +0900

    Update 01_login.py

--- src/01_login.py
+++ src/01_login.py
@@ -1,15 +1,9 @@
 import streamlit as st
-import yt_dlp
-ydl_opts = {'format': 'best','outtmpl':'video.%(ext)s',}
-text = st.text_input("URL：")
-text1 = ""
-if text != text1:
-    import os
-    if os.path.isfile('video.mp4'):
-        os.remove('video.mp4')
-    text1 = text
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(text)
-        video_file = open("video.mp4", "rb")
-        video_bytes = video_file.read()
-        st.video(video_bytes)
+import urllib.request
+
+def fetch_html(url: str) -> str:
+    with urllib.request.urlopen(url) as res:
+        html = res.read().decode()
+    return html
+
+st.markdown(fetch_html('https://www.google.co.jp/'),unsafe_allow_html=True)
